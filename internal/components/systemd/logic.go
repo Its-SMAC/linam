ca9e6bd5c2732911540d1218a6a0c43fb1e3e7db
Its-SMAC <196476326+Its-SMAC@users.noreply.github.com>
2026-08-01 22:18:27 +0100

    Update logic.go

--- internal/components/systemd/logic.go
+++ internal/components/systemd/logic.go
@@ -1,3 +1,19 @@
 package systemd
 
 // Core logic for the systemd feature.
+
+func create() {}
+
+func start() {}
+
+func stop() {}
+
+func enable() {}
+
+func disable() {}
+
+func restart() {}
+
+func edit() {}
+
+func status() {}
